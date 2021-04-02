2560150bd77801b4f5bc2618a65f030b56b01c87
apietryga <antek.pietryga@gmail.com>
2021-04-02 12:28:20 +0200

    script.js commented

--- lekcja9/script.js
+++ lekcja9/script.js
@@ -1,3 +1,4 @@
+  // TWORZENIE TABLICY OBIEKTÓW (PUNKT 1)
   const views = [
     {
       title : "Zachód słońca pod palmami",
@@ -13,30 +14,28 @@
     }
   ];
 
-  let currentSlide = 0;
-
+  // FUNKCJA WYŚWIETLAJĄCA KONKRETNY ELEMENT (PUNKT 2)
   function displayView(index){
     const titleField = document.querySelector("h2");
     titleField.innerHTML = views[index].title;
-
+    
     const imgField = document.querySelector(".content");
     imgField.innerHTML = "";
-
+    
     const img = document.createElement("img");
     img.src = "img/" + views[index].src;
     imgField.append(img);
   }
+  
+  // ZMIENNA ZMIENIAJĄCA NUMERY SLAJDÓW (PUNKT 3)
+  let currentSlide = 0;
 
+  // WYWOŁANIE FUNKCJI Z PUNKTU 2 Z DOMYŚLNĄ WARTOŚCIĄ
+  //  (robimy to po to, żeby po załadowaniu strony wyświetlił
+  //  się jakiś obrazek)
   displayView(currentSlide);
 
-
-  //  USTAWIAMY KLIKALNE PRZYCISKI 
-  const leftArr = document.querySelector(".left");
-  leftArr.onclick = () => {changeSlide("left")};
-  const rightArr = document.querySelector(".right");
-  rightArr.onclick = () => {changeSlide("right")};
-
-  // PRZEŁĄCZANIE SLAJDÓW
+  // PRZEŁĄCZANIE SLAJDÓW (PUNKT 4)
   function changeSlide(direction){
     if(direction == "left"){
       currentSlide--;
@@ -53,3 +52,12 @@
     console.log(currentSlide);
     displayView(currentSlide);
   }
+
+  //  POBIERAMY HTML'OWE STRZAŁKI (PUNKT 5)
+  const leftArr = document.querySelector(".left");
+  // I DOPISUJEMY DO NICH FUNKCJĘ PO KLIKNIĘCIU
+  leftArr.onclick = () => {changeSlide("left")};
+  const rightArr = document.querySelector(".right");
+  rightArr.onclick = () => {changeSlide("right")};
+
+
