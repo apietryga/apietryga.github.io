187557607a9cb2f544fdf2f905111b1f20554d0a
apietryga <antek.pietryga@gmail.com>
2022-12-07 21:00:31 +0100

    sources replaced

--- vue/src/views/ProjectView.vue
+++ vue/src/views/ProjectView.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="projects">
-    <h2>SINGLE PROJECT</h2>
-    <h3>{{ project.name }}</h3>
+  <div class="project">
+
+    <div class="topBackground" :style="'background: linear-gradient('+project.color+', transparent)'">
+    </div>
+
+
+    <h2>{{ project.name }}</h2>
+    <p>{{ project.lang.pl.desc}}</p>
+    
+    <img 
+      :src="'/img/contents/' + project.img" 
+      :alt="project.name + ' image'" 
+      style="width:10rem;height:10rem;">
+
+    <!-- <p>{{ project.lang.pl.content}}</p> -->
+    <p v-html="project.lang.pl.content"></p>
+
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" scoped>
   import { useDataStore } from '@/stores'
 
   export default {
@@ -23,6 +37,22 @@
         return this.dataStore.projects
         .find( ( project : any) => project.url == location.pathname.split("/")[2] )
       }
-    }
+    },
+    mounted(){
+      console.log(this.project)
+    },
   }
 </script>
+
+<style lang="scss">
+.topBackground{
+  // border:2px dashed Green;
+  width:100%;
+  height:50vh;
+  position:absolute;
+  top:-4rem;
+  z-index:-1;
+  left:0;
+}
+
+</style>
