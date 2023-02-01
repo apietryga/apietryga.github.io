27cb80d8e692af78d9fc5770e6c56a409c460f01
apietryga <antek.pietryga@gmail.com>
2023-02-02 00:40:47 +0100

    pretty header done

--- components/nav/header.vue
+++ components/nav/header.vue
@@ -2,7 +2,7 @@
   <header class='navHeader'>
     <div class="searchAndMenu">
       <div class="menu">
-        <input type="checkbox" id="toggler">
+        <input type="checkbox" id="toggler" v-model="toggler">
         <label for="toggler" class="toggler">
           <div></div>
           <div></div>
@@ -10,15 +10,23 @@
         </label>
         <nav> 
           <NuxtLink to="/">
-            <img src="/img/page/nav_home.svg" :alt="pageBuild[language].nav.home">
+            <svg width="1.5rem" height="1.5rem" version="1.1" viewBox="0 0 6.031 6.031">
+              <path d="m0 2.9826h6.0312l-3.0575-2.9626zm0.79284 2e-3v3.022h1.5978v-1.4066h1.2392v1.4066h1.5286v-3.022z" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width=".11559" fill="var(--font-primary-color)"/>
+            </svg>
             <span>{{ pageBuild[language].nav.home }}</span>
           </NuxtLink>
           <NuxtLink to="/projects">
-            <img src="/img/page/nav_projects.svg" alt="{{ pageBuild[language].nav.projects }}">
+            <svg width="1.5rem" height="1.5rem" version="1.1" viewBox="0 0 7.476 7.476">
+              <path d="m0.00496 1.3496-0.0040162-0.89572c0.043515-0.25255 0.18469-0.41558 0.46192-0.45388h3.4744c0.20556 0.012296 0.43234 0.15777 0.46995 0.44183v0.90777z" fill="var(--font-primary-color)"/>
+              <path d="m7.8738e-6 1.5698v5.399c-0.0014764 0.24134 0.20483 0.44241 0.47956 0.44235h6.4988c0.2905 5.423e-4 0.49807-0.2021 0.49713-0.50229v-4.8357c0.00508-0.30561-0.25317-0.5015-0.55811-0.50023z" fill="var(--font-primary-color)"/>
+            </svg>
             <span>{{ pageBuild[language].nav.projects }}</span>
           </NuxtLink>
           <NuxtLink to="/contact">
-            <img src="/img/page/nav_contact.svg" alt="{{ pageBuild[language].nav.contact }}">
+            <svg width="1.5rem" height="1.5rem" version="1.1" viewBox="0 0 7.899 7.899">
+              <path d="m4.5916e-6 0.14748v7.5169c-9.25e-4 0.14767 0.13805 0.28619 0.28384 0.28619h7.3205c0.17243-6.71e-4 0.29333-0.13744 0.29436-0.29106v-7.499l-3.96 4.8137s-3.8684-4.8401-3.9387-4.8267z" fill="var(--font-primary-color)" style="paint-order:normal"/>
+              <path d="m0.033107 0 3.8891 4.716 3.9468-4.7z" fill="var(--font-primary-color)"/>
+            </svg>
             <span>{{ pageBuild[language].nav.contact }}</span>
           </NuxtLink>
           <select name="language">
@@ -31,12 +39,22 @@
       <div class="searchContainer">
         <form action="/projects" method="get">
           <label for="search">
-            <img src="/img/page/nav_lupa.svg" alt="SEARCH" />
+            <svg width="1.5rem" height="1.5rem" version="1.1" viewBox="0 0 13.425 13.229">
+              <g transform="translate(48.371 -75.481)" stroke="var(--font-primary-color)" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="-42.914" cy="81.458" r="2.9783" fill="none" style="paint-order:normal"/>
+                <rect transform="rotate(-52.957)" x="-91.356" y="17.877" width=".15743" height="4.127" fill="var(--font-primary-color)" style="paint-order:normal"/>
+              </g>
+            </svg>
           </label>
           <input type="text" name="q" id="search" list="suggest" placeholder="Search" />
           <div class="bg"></div>
           <div class="abort">
-            <img src="/img/page/nav_x.svg" alt="ABORT SEARCHING" />
+            <svg width="1.5rem" height="1.5rem" version="1.1" viewBox="0 0 13.425 13.229">
+              <g transform="translate(48.371 -75.481)" fill="none" stroke="var(--font-primary-color)" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m-45.655 78.137 7.9677 7.841"/>
+                <path d="m-37.699 78.09-7.9446 7.8871"/>
+              </g>
+            </svg>
           </div>          
         </form>
         <div class="suggestions"></div>
@@ -50,7 +68,17 @@
 
 <script>
   export default {
-    data(){ return this.$appData },
+    data(){ 
+      return {
+        ...this.$appData,
+        toggler: 'off',
+      } 
+    },
+    watch:{
+      '$route.fullPath'(newValue){
+        this.toggler = 'off'
+      }
+    },
   }
 </script>
 
@@ -149,20 +177,6 @@
           }
         }
         nav{
-          @keyframes hideMenu {
-            0%{
-              opacity:1;
-              padding:.5rem .5rem;
-            }
-            30%{
-              opacity:0;
-              padding:0 .5rem;
-            }
-            100%{
-              opacity:0;
-              padding:0 .5rem;
-            }        
-          }  
           top:var(--navHeaderHeight);
           transition:1s;
           position:absolute;
@@ -179,27 +193,28 @@
           }
           a{
             @extend %actionColors;
-            display:flex;
             align-items: center;
             border-radius:1rem;
-            img{
-              @include svgIcon(20%);
-            }
-            span{
-              padding-right:1.5rem;
+            color: var(--font-primary-color);
+            display:flex;
+            font-size:.8rem;
+            line-height: .7rem;
+            margin-left:1rem;
+            svg{
+              padding:5px;
             }
           }
           select{
-            padding:.5rem;
+            animation: none;
+            background-color: transparent;
+            border:none;
+            color:var(--font-primary-color);
             cursor:pointer;
             margin:0 1rem;
             max-width:fit-content;
-            animation: none;
-            color:inherit;
-            background-color: transparent;
-            white-space: normal;
             outline: none;
-            border:none;
+            padding:.5rem;
+            white-space: normal;
             option{
               padding:0;
               color:#000;
@@ -245,23 +260,17 @@
           label,.abort{
             cursor:pointer;
             display:flex;
+            align-items:center;
             position:relative;
             z-index:1;
-          }
-          img{
-            @include svgIcon(0%);
-            transform: scale(0.6);
-            margin:0;
-            cursor:pointer;
+            width:1.8rem;
+            padding:0 .3rem;
           }
           .abort{
             opacity:0;
-            left:(-$iconSize);
             transition: .5s;
             width:0;
-            img{
-              width:0;
-            }
+            padding:0;
           }
           input:focus{
             width:7rem;
@@ -269,10 +278,8 @@
           input:focus ~ .abort{
             opacity:1;
             left:0;
-            width:$iconSize;
-            img{
-              width:$iconSize;
-            }
+            padding:0 .3rem;
+            width:1.8rem;
           }
           input:focus ~ .bg{
             background:var(--hover-color);
@@ -341,7 +348,7 @@
                 padding:0;
                 margin:0 .5rem;
               }
-              img{
+              svg{
                 display:none;
               }
             }
