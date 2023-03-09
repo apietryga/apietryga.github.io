63afa38ffd3c1858639191153cf1be13bfa4338c
apietryga <antek.pietryga@gmail.com>
2023-03-09 21:40:58 +0100

    test deploy

--- deploy.mjs
+++ deploy.mjs
@@ -1,19 +1,43 @@
 import { execa } from "execa";
+const folderName = ".output/public";
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    await execa(`git checkout --orphan gh-pages`);
+
     console.log("Building started...");
-    await execa("npm", ["run", "generate"]);
-    // Understand if it's dist or build folder
-    // const folderName = existsSync("dist") ? "dist" : "build";
-    const folderName = ".output/public";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+
+    await execa(`npm run generate`);
+    await execa(`git --work-tree ${ folderName } add --all`);
+    await execa(`git --work-tree ${ folderName } commit -m gh-pages`);
+
     console.log("Pushing to gh-pages...");
-    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    // await execa("rm", ["-r", folderName]);
-    await execa("git", ["checkout", "-f", "master"]);
-    await execa("git", ["branch", "-D", "gh-pages"]);
+
+    await execa(`git push origin HEAD:gh-pages --force`);
+    await execa(`git checkout -f master`);
+    await execa(`git branch -D gh-pages`);
+ 
     console.log("Successfully deployed, check your settings");
+ 
   } catch (e) { console.log({ e }); process.exit(1) }
 })();
+
+// // WORKING EXAMPLE
+// import { execa } from "execa";
+// (async () => {
+//   try {
+//     await execa("git", ["checkout", "--orphan", "gh-pages"]);
+//     console.log("Building started...");
+//     await execa("npm", ["run", "generate"]);
+//     // Understand if it's dist or build folder
+//     // const folderName = existsSync("dist") ? "dist" : "build";
+//     const folderName = ".output/public";
+//     await execa("git", ["--work-tree", folderName, "add", "--all"]);
+//     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+//     console.log("Pushing to gh-pages...");
+//     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+//     // await execa("rm", ["-r", folderName]);
+//     await execa("git", ["checkout", "-f", "master"]);
+//     await execa("git", ["branch", "-D", "gh-pages"]);
+//     console.log("Successfully deployed, check your settings");
+//   } catch (e) { console.log({ e }); process.exit(1) }
+// })();
