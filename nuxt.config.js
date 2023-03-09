f8f5e014c9c63fa78fbdb07d0957bfe09bf0303d
apietryga <antek.pietryga@gmail.com>
2023-03-09 21:26:01 +0100

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,7 @@
 export default defineNuxtConfig({
-  css: [
-    "@/assets/base.css",
-    "@/assets/animations.css",
-  ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: { additionalData: '@import "@/assets/base.scss";' },
-      },
-    },
-  },
-  transpile: [
-    "three"
-  ],
-  render: { spaScripts: false },
+  css: [ "@/assets/base.css", "@/assets/animations.css" ],
   experimental: { payloadExtraction: false },
+  render: { spaScripts: false },
+  transpile: [ "three" ],
+  vite: { css: { preprocessorOptions: { scss: { additionalData: '@import "@/assets/base.scss";' }}}},
 })
