e7b760240a5969830b36384bc0e5233bdf282ef5
apietryga <antek.pietryga@gmail.com>
2023-03-09 21:16:28 +0100

    generate full html

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,37 +6,13 @@ export default defineNuxtConfig({
   vite: {
     css: {
       preprocessorOptions: {
-        scss: {
-          additionalData: '@import "@/assets/base.scss";',
-        },
+        scss: { additionalData: '@import "@/assets/base.scss";' },
       },
     },
   },
   transpile: [
     "three"
   ],
-  // modules: [
-  //   ['@nuxtjs/google-fonts', {
-  //     families: {
-  //       'Nunito': true,
-  //       download: true,
-  //       inject: true
-  //     }
-  //   }]
-  // ]
-  // buildModules: [
-  //   '@nuxtjs/google-fonts'
-  // ],
-  // googleFonts: {
-  //   families: {
-  //     Ubuntu: true,
-  //     // Roboto: true,
-  //     // 'Josefin+Sans': true,
-  //     // Lato: [100, 300],
-  //     // Raleway: {
-  //     //   wght: [100, 400],
-  //     //   ital: [100]
-  //     // },
-  //   }
-  // }
+  render: { spaScripts: false },
+  experimental: { payloadExtraction: false },
 })
