5d8ab9aa3c8d6de75d9afd5de8a33cff1ce44a5e
apietryga <antek.pietryga@gmail.com>
2023-03-10 18:50:14 +0100

    implement sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   css: [ "@/assets/base.css", "@/assets/animations.css" ],
   experimental: { payloadExtraction: false },
   render: { spaScripts: false },
+  modules: [ '@nuxtjs/sitemap' ], // sitemap must be at the end
   transpile: [ "three" ],
   vite: { css: { preprocessorOptions: { scss: { additionalData: '@import "@/assets/base.scss";' }}}},
 })
