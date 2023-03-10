348ca4b46cc7cd2ea3381ce64a325d5ddc243714
apietryga <antek.pietryga@gmail.com>
2023-03-10 19:01:38 +0100

    sitemap fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,13 @@
 export default defineNuxtConfig({
   css: [ "@/assets/base.css", "@/assets/animations.css" ],
   experimental: { payloadExtraction: false },
+  modules: [ 'nuxt-simple-sitemap' ], // sitemap must be at the end
   render: { spaScripts: false },
-  modules: [ '@nuxtjs/sitemap' ], // sitemap must be at the end
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.ORIGIN || 'https://apietryga.github.io',
+    }
+  },
   transpile: [ "three" ],
   vite: { css: { preprocessorOptions: { scss: { additionalData: '@import "@/assets/base.scss";' }}}},
 })
