9b5708e73abd25c477bf05f57cacdf4dd0bec45b
apietryga <antek.pietryga@gmail.com>
2023-05-04 23:27:04 +0200

    coding section mobile

---
+++ components/home/spacer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="spacer">
+    SPACER
+
+  </div>
+</template>
+
+<style lang="scss">
+.spacer{
+  border:2px dashed red;
+  // height: 50vh;
+  // padding:25vh 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -18,77 +18,95 @@
             </ul>
           </div>
           <section class="card__wrapper">
-            <NuxtLink 
+            <!-- <NuxtLink 
               :key="element"
               :to="'/projects/' + element.url" 
               v-for="[index, element] in getProjectsByNames(recomended.code).entries()" 
+            > -->
+            <NuxtLink 
+              v-for="[index, element] in getProjectsByNames(recomended.code).entries()" 
+              :to="'/projects/' + element.url" 
+              :data-aos-delay="100*index"
+              data-aos="fade-right" 
+              :key="index"
+              class="card" 
             >
-              <article class="card" data-aos="fade-right" :data-aos-delay="100*index">
-                <header class="card__header">
-                  <h3>{{ element.name }}</h3>
-                  <p>{{ element.lang[language].desc }}</p>
-                </header>
-                <picture class="card__picture">
-                  <img :src="'/img/contents/' + element.img" 
-                    :alt="element.name" 
-                    height="50"
-                    width="50"
-                  >
-                </picture>
-              </article>
+              <!-- <article class="card" data-aos="fade-right" :data-aos-delay="100*index"> -->
+              <header class="card__header">
+                <h3>{{ element.name }}</h3>
+                <p>{{ element.lang[language].desc }}</p>
+              </header>
+              <picture class="card__picture">
+                <img :src="'/img/contents/' + element.img" 
+                  :alt="element.name" 
+                  height="50"
+                  width="50"
+                >
+              </picture>
+              <!-- </article> -->
             </NuxtLink>
           </section>
           <footer data-aos="fade-up">
-            <!-- <NuxtLink to="/projects">{{ lang[language].content[7] }}</NuxtLink> -->
             <NuxtLink to="/projects" class="btn">{{ lang[language].content[7] }}</NuxtLink>
           </footer>
         </div>
       </main>
+
+      <homeSpacer />
+      
       <article id="teaching">
-        <!-- <div class="mask top"></div> -->
-        <!-- <div class="stopper"></div> -->
         <header>
           <h2 data-aos="fade-up">{{lang[language].content[8]}}</h2>
           <p>{{lang[language].content[9]}}</p>  
         </header>
         <div class="content">
           <p>{{lang[language].content[10]}}</p>
-          <section>
-            <NuxtLink v-for="element in getProjectsByNames(recomended.teaching)" :to="'/projects/' + element.url" :key="element">
-              <article>
-                <header>
+          <section class="card__wrapper">
+            <NuxtLink 
+              v-for="element in getProjectsByNames(recomended.teaching)" 
+              :to="'/projects/' + element.url" :key="element"
+              class="card">
+              <!-- <article class="card"> -->
+                <header class="card__header">
                   <h3>{{ element.name }}</h3>
                   <p>{{ element.lang[language].desc }}</p>
                 </header>
-                <img :src="'/img/contents/' + element.img" :alt="element.name">
-              </article>
+                <picture class="card__picture">
+                  <img :src="'/img/contents/' + element.img" :alt="element.name" height="50" width="50">
+                </picture>
+              <!-- </article> -->
             </NuxtLink>
             <div></div>
-            <footer>
-              <NuxtLink to="/projects">{{ lang[language].content[11] }}</NuxtLink>
-            </footer>  
           </section>
+          <footer>
+            <NuxtLink to="/projects">{{ lang[language].content[11] }}</NuxtLink>
+          </footer>  
         </div>
       </article>
       <article id="business">
         <div class="photo me"></div>
         <h2>{{ lang[language].content[12] }}</h2>
         <p>{{ lang[language].content[13] }}</p>
-        <section>
-          <NuxtLink v-for="element in getProjectsByNames(recomended.business)" :to="'/projects/' + element.url" :key="element">
-            <article>
-              <header>
+        <section class="card__wrapper">
+          <NuxtLink 
+            v-for="element in getProjectsByNames(recomended.business)" 
+            :to="'/projects/' + element.url" :key="element"
+            class="card">
+            <!-- <article class="card"> -->
+              <header class="card__header">
                 <h3>{{element.name}}</h3>
                 <p>{{element.lang[language].desc}}</p>
               </header>
-              <img :src="'/img/contents/' + element.img" :alt="element.name">
-            </article>
+              <picture class="card__picture">
+                <img :src="'/img/contents/' + element.img" :alt="element.name" height="50" width="50">
+              </picture>
+            <!-- </article> -->
           </NuxtLink>
           <div></div>
-          <footer>
-            <NuxtLink :to="'/projects?q=' + lang[language].content[15]">{{ lang[language].content[14] }}</NuxtLink>
-          </footer>  
         </section>
+        <footer>
+          <NuxtLink :to="'/projects?q=' + lang[language].content[15]">{{ lang[language].content[14] }}</NuxtLink>
+        </footer>  
       </article>
     </div>
   </div>
@@ -128,113 +146,113 @@
       z-index:-10;
     }
   }
-  %rowSection{
-    flex:1;
-    max-width:80%;
-    left:0;
-    display:grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-template-rows:3fr 1fr;
-    a{
-      display:flex;
-      justify-content: center;
-      align-items: center;
-      margin:2rem 0;
-      &:nth-child(1){
-        text-align: right;
-      }
-      &:nth-child(2){
-        img{
-          // max-width:5rem;
-          // max-height:5rem;   
-          width:5rem;
-          height:5rem;  
-        }
-      }
-      &:nth-child(3){
-        text-align: left;
-      }
-      article{
-        display:flex;
-        flex-direction: column-reverse;
-        justify-content: center;
-        align-items: center;
-        header{
-          padding:0;
-          h2,h3,p{
-            margin:0;
-            width:100%;
-            display:inline-block;
-            padding:0;
-            text-align: center;
-          }
-        }
-        img{
-          background-color: var(--bright-primary);
-          border-radius:50%;
-          box-shadow: 0 0 5px var(--bright-primary);
-          width:4rem;
-          height:4rem;
-          margin:.8rem 0;
-          padding:.5rem;
-        }
-      }
-    }
-    footer{
-      height:2rem;
-      z-index: 1;
-      display:flex;
-      justify-content: center;
-      //height:100%;
-      // display:flex;
-      align-items: flex-end;
-      a{
-        text-align: center;
-        color:inherit;
-        margin:0;
-        &:hover{
-          color:rgb(146, 129, 129);
-          transition: .5s;
-        }
-      }
-    }
-    @media (max-width:720px){
-      grid-template-columns:1fr;
-      grid-template-rows: 1fr 1fr 1fr;          
-      a{
-        width:60%;
-        margin:auto;
-        img{
-          max-width:5rem!important;
-          max-height:5rem!important;     
-        }
-      }
-      footer{
-        a{
-          text-align: center!important;
-          margin-top:4rem;
-        }
-      }
-    }
-  }
-  .mask{
-    // background:red;
-    clip-path: polygon(50% 100%, 100% 0%, 100% 100%, 0 100%, 0 0);
-    height:calc(10vh + 5px);
-    position:relative;
-    bottom:-.1rem;
-    width:100%;
-    &.light{
-      background-color:var(--backgroundColor);
-    }
-    &.dark{
-      clip-path: polygon(100% 0, 0 0, 50% 100%);
-      bottom:calc(-10vh - 4px);
-      // background-color: #040003;
-        background:red;
+  // %rowSection{
+  //   flex:1;
+  //   max-width:80%;
+  //   left:0;
+  //   display:grid;
+  //   grid-template-columns: 1fr 1fr 1fr;
+  //   grid-template-rows:3fr 1fr;
+  //   a{
+  //     display:flex;
+  //     justify-content: center;
+  //     align-items: center;
+  //     margin:2rem 0;
+  //     &:nth-child(1){
+  //       text-align: right;
+  //     }
+  //     &:nth-child(2){
+  //       img{
+  //         // max-width:5rem;
+  //         // max-height:5rem;   
+  //         width:5rem;
+  //         height:5rem;  
+  //       }
+  //     }
+  //     &:nth-child(3){
+  //       text-align: left;
+  //     }
+  //     article{
+  //       display:flex;
+  //       flex-direction: column-reverse;
+  //       justify-content: center;
+  //       align-items: center;
+  //       header{
+  //         padding:0;
+  //         h2,h3,p{
+  //           margin:0;
+  //           width:100%;
+  //           display:inline-block;
+  //           padding:0;
+  //           text-align: center;
+  //         }
+  //       }
+  //       img{
+  //         background-color: var(--bright-primary);
+  //         border-radius:50%;
+  //         box-shadow: 0 0 5px var(--bright-primary);
+  //         width:4rem;
+  //         height:4rem;
+  //         margin:.8rem 0;
+  //         padding:.5rem;
+  //       }
+  //     }
+  //   }
+  //   footer{
+  //     height:2rem;
+  //     z-index: 1;
+  //     display:flex;
+  //     justify-content: center;
+  //     //height:100%;
+  //     // display:flex;
+  //     align-items: flex-end;
+  //     a{
+  //       text-align: center;
+  //       color:inherit;
+  //       margin:0;
+  //       &:hover{
+  //         color:rgb(146, 129, 129);
+  //         transition: .5s;
+  //       }
+  //     }
+  //   }
+  //   @media (max-width:720px){
+  //     grid-template-columns:1fr;
+  //     grid-template-rows: 1fr 1fr 1fr;          
+  //     a{
+  //       width:60%;
+  //       margin:auto;
+  //       img{
+  //         max-width:5rem!important;
+  //         max-height:5rem!important;     
+  //       }
+  //     }
+  //     footer{
+  //       a{
+  //         text-align: center!important;
+  //         margin-top:4rem;
+  //       }
+  //     }
+  //   }
+  // }
+  // .mask{
+  //   // background:red;
+  //   clip-path: polygon(50% 100%, 100% 0%, 100% 100%, 0 100%, 0 0);
+  //   height:calc(10vh + 5px);
+  //   position:relative;
+  //   bottom:-.1rem;
+  //   width:100%;
+  //   &.light{
+  //     background-color:var(--backgroundColor);
+  //   }
+  //   &.dark{
+  //     clip-path: polygon(100% 0, 0 0, 50% 100%);
+  //     bottom:calc(-10vh - 4px);
+  //     // background-color: #040003;
+  //       background:red;
 
-    }
-  }
+  //   }
+  // }
   #code{
     position:relative;
     .codeprojects{
@@ -276,36 +294,36 @@
         a{
           max-width: 25%;
           // article:nth-child(1){
-          &:nth-child(2){
-          //   // border:2px dashed blue;
-            transform: scale(1.2);
-          //   // align-items: center;
-          //   // color:var(--font-primary-color);
-          //   // border:.25rem solid var(--bright-primary);
-          //   // border-radius:.5rem;
-          //   // display:flex;
-          //   // flex-direction: row-reverse;
-          //   // padding:.5rem;
-          //   // margin:0 .5rem;
-          //   // header{
-          //   //   // margin-left:.5rem;
-          //   //   // h3{
-          //   //   //   margin:0;
-          //   //   // }
-          //   //   p{
-          //   //     font-size:.8rem;
-          //   //     line-height: .8rem;
-          //   //   }
-          //   // }
-          //   // picture{
-          //   //   padding:.25rem;
-          //   //   background: var(--font-primary-color);
-          //   //   display:flex;
-          //   //   align-items: center;
-          //   //   justify-content: center;
-          //   //   border-radius:50%;
-          //   // }
-          }
+          // &:nth-child(2){
+          // //   // border:2px dashed blue;
+          //   transform: scale(1.2);
+          // //   // align-items: center;
+          // //   // color:var(--font-primary-color);
+          // //   // border:.25rem solid var(--bright-primary);
+          // //   // border-radius:.5rem;
+          // //   // display:flex;
+          // //   // flex-direction: row-reverse;
+          // //   // padding:.5rem;
+          // //   // margin:0 .5rem;
+          // //   // header{
+          // //   //   // margin-left:.5rem;
+          // //   //   // h3{
+          // //   //   //   margin:0;
+          // //   //   // }
+          // //   //   p{
+          // //   //     font-size:.8rem;
+          // //   //     line-height: .8rem;
+          // //   //   }
+          // //   // }
+          // //   // picture{
+          // //   //   padding:.25rem;
+          // //   //   background: var(--font-primary-color);
+          // //   //   display:flex;
+          // //   //   align-items: center;
+          // //   //   justify-content: center;
+          // //   //   border-radius:50%;
+          // //   // }
+          // }
         }
       }
       .techCloud{
@@ -367,15 +385,15 @@
     display:flex;
     flex-direction: column;
     // margin-top:calc(10vh + 5px);
-    .stopper{
-      position:absolute;
-      background-image: url("/img/frontPage/projects_footer.webp");
-      background-size:auto 100%;
-      background-repeat: no-repeat;
-      background-position: center;
-      height:15rem;
-      width:100%;
-    }
+    // .stopper{
+    //   position:absolute;
+    //   background-image: url("/img/frontPage/projects_footer.webp");
+    //   background-size:auto 100%;
+    //   background-repeat: no-repeat;
+    //   background-position: center;
+    //   height:15rem;
+    //   width:100%;
+    // }
     > header{
       // margin-top:13rem;
       h2{
@@ -397,21 +415,21 @@
       display:flex;
       flex-direction: column;
       align-items: center;
-      p{
-        flex:1;
-        margin:0 1.5rem 0rem 1.5rem;
-        text-align: center;
-      }
+      // p{
+      //   flex:1;
+      //   // margin:0 1.5rem 0rem 1.5rem;
+      //   text-align: center;
+      // }
       section{
         padding-bottom:0;
         width:80%;
-        @extend %rowSection;
+        // @extend %rowSection;
       }
 
     }
-    .top{
-      top:calc(-10vh - 4px);
-    }
+    // .top{
+    //   top:calc(-10vh - 4px);
+    // }
   }
   #business{
     background: var(--bright-primary);
@@ -435,7 +453,7 @@
       line-height:1.5rem;
     }
     section{
-      @extend %rowSection;
+      // @extend %rowSection;
       padding:0 0 2rem 0;
       a{
         margin-top:0;
@@ -462,7 +480,7 @@
       >section{
         flex-direction: column;
         padding:0 2rem;
-        a{ max-width: 100%; }
+        a{ max-width: 60%; }
       }
       // footer{
       //   // text-align: center;
