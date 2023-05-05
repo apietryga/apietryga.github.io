6acbb0e059594f6e37729d8d6399aaf9fb0a9bfd
apietryga <antek.pietryga@gmail.com>
2023-05-05 16:44:05 +0200

    code animation upgrade

--- components/home/spacer.vue
+++ components/home/spacer.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="spacer">
-    SPACER
+    <!-- SPACER -->
 
   </div>
 </template>
 
 <style lang="scss">
 .spacer{
-  border:2px dashed red;
-  // height: 50vh;
+  // border:2px dashed red;
+  height: 50vh;
   // padding:25vh 0;
+  // background-image: url("/img/contents/projects_footer.webp");
+  background-image: url("/img/frontPage/projects_footer.webp");
+  background-attachment: fixed;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -18,11 +18,6 @@
             </ul>
           </div>
           <section class="card__wrapper">
-            <!-- <NuxtLink 
-              :key="element"
-              :to="'/projects/' + element.url" 
-              v-for="[index, element] in getProjectsByNames(recomended.code).entries()" 
-            > -->
             <NuxtLink 
               v-for="[index, element] in getProjectsByNames(recomended.code).entries()" 
               :to="'/projects/' + element.url" 
@@ -31,7 +26,6 @@
               :key="index"
               class="card" 
             >
-              <!-- <article class="card" data-aos="fade-right" :data-aos-delay="100*index"> -->
               <header class="card__header">
                 <h3>{{ element.name }}</h3>
                 <p>{{ element.lang[language].desc }}</p>
@@ -43,7 +37,6 @@
                   width="50"
                 >
               </picture>
-              <!-- </article> -->
             </NuxtLink>
           </section>
           <footer data-aos="fade-up">
@@ -65,16 +58,15 @@
             <NuxtLink 
               v-for="element in getProjectsByNames(recomended.teaching)" 
               :to="'/projects/' + element.url" :key="element"
-              class="card">
-              <!-- <article class="card"> -->
-                <header class="card__header">
-                  <h3>{{ element.name }}</h3>
-                  <p>{{ element.lang[language].desc }}</p>
-                </header>
-                <picture class="card__picture">
-                  <img :src="'/img/contents/' + element.img" :alt="element.name" height="50" width="50">
-                </picture>
-              <!-- </article> -->
+              class="card"
+            >
+              <header class="card__header">
+                <h3>{{ element.name }}</h3>
+                <p>{{ element.lang[language].desc }}</p>
+              </header>
+              <picture class="card__picture">
+                <img :src="'/img/contents/' + element.img" :alt="element.name" height="50" width="50">
+              </picture>
             </NuxtLink>
             <div></div>
           </section>
@@ -91,16 +83,15 @@
           <NuxtLink 
             v-for="element in getProjectsByNames(recomended.business)" 
             :to="'/projects/' + element.url" :key="element"
-            class="card">
-            <!-- <article class="card"> -->
-              <header class="card__header">
-                <h3>{{element.name}}</h3>
-                <p>{{element.lang[language].desc}}</p>
-              </header>
-              <picture class="card__picture">
-                <img :src="'/img/contents/' + element.img" :alt="element.name" height="50" width="50">
-              </picture>
-            <!-- </article> -->
+            class="card"
+          >
+            <header class="card__header">
+              <h3>{{element.name}}</h3>
+              <p>{{element.lang[language].desc}}</p>
+            </header>
+            <picture class="card__picture">
+              <img :src="'/img/contents/' + element.img" :alt="element.name" height="50" width="50">
+            </picture>
           </NuxtLink>
           <div></div>
         </section>
@@ -146,113 +137,6 @@
       z-index:-10;
     }
   }
-  // %rowSection{
-  //   flex:1;
-  //   max-width:80%;
-  //   left:0;
-  //   display:grid;
-  //   grid-template-columns: 1fr 1fr 1fr;
-  //   grid-template-rows:3fr 1fr;
-  //   a{
-  //     display:flex;
-  //     justify-content: center;
-  //     align-items: center;
-  //     margin:2rem 0;
-  //     &:nth-child(1){
-  //       text-align: right;
-  //     }
-  //     &:nth-child(2){
-  //       img{
-  //         // max-width:5rem;
-  //         // max-height:5rem;   
-  //         width:5rem;
-  //         height:5rem;  
-  //       }
-  //     }
-  //     &:nth-child(3){
-  //       text-align: left;
-  //     }
-  //     article{
-  //       display:flex;
-  //       flex-direction: column-reverse;
-  //       justify-content: center;
-  //       align-items: center;
-  //       header{
-  //         padding:0;
-  //         h2,h3,p{
-  //           margin:0;
-  //           width:100%;
-  //           display:inline-block;
-  //           padding:0;
-  //           text-align: center;
-  //         }
-  //       }
-  //       img{
-  //         background-color: var(--bright-primary);
-  //         border-radius:50%;
-  //         box-shadow: 0 0 5px var(--bright-primary);
-  //         width:4rem;
-  //         height:4rem;
-  //         margin:.8rem 0;
-  //         padding:.5rem;
-  //       }
-  //     }
-  //   }
-  //   footer{
-  //     height:2rem;
-  //     z-index: 1;
-  //     display:flex;
-  //     justify-content: center;
-  //     //height:100%;
-  //     // display:flex;
-  //     align-items: flex-end;
-  //     a{
-  //       text-align: center;
-  //       color:inherit;
-  //       margin:0;
-  //       &:hover{
-  //         color:rgb(146, 129, 129);
-  //         transition: .5s;
-  //       }
-  //     }
-  //   }
-  //   @media (max-width:720px){
-  //     grid-template-columns:1fr;
-  //     grid-template-rows: 1fr 1fr 1fr;          
-  //     a{
-  //       width:60%;
-  //       margin:auto;
-  //       img{
-  //         max-width:5rem!important;
-  //         max-height:5rem!important;     
-  //       }
-  //     }
-  //     footer{
-  //       a{
-  //         text-align: center!important;
-  //         margin-top:4rem;
-  //       }
-  //     }
-  //   }
-  // }
-  // .mask{
-  //   // background:red;
-  //   clip-path: polygon(50% 100%, 100% 0%, 100% 100%, 0 100%, 0 0);
-  //   height:calc(10vh + 5px);
-  //   position:relative;
-  //   bottom:-.1rem;
-  //   width:100%;
-  //   &.light{
-  //     background-color:var(--backgroundColor);
-  //   }
-  //   &.dark{
-  //     clip-path: polygon(100% 0, 0 0, 50% 100%);
-  //     bottom:calc(-10vh - 4px);
-  //     // background-color: #040003;
-  //       background:red;
-
-  //   }
-  // }
   #code{
     position:relative;
     .codeprojects{
@@ -277,54 +161,14 @@
         }
       }
       footer{
-        display:flex;
-        // align-items: flex-start;
         align-items: center;
+        display:flex;
         justify-content: center;
+        margin:1rem 0 3rem 0;
         width:100%;
-        // padding-left:2rem;
-        margin-top:1rem;
       }
       section{
-        // flex:1;
-        // display:flex;
-        // justify-content: flex-start;
-        // margin:3rem 0;
         margin-top:3rem;
-        a{
-          max-width: 25%;
-          // article:nth-child(1){
-          // &:nth-child(2){
-          // //   // border:2px dashed blue;
-          //   transform: scale(1.2);
-          // //   // align-items: center;
-          // //   // color:var(--font-primary-color);
-          // //   // border:.25rem solid var(--bright-primary);
-          // //   // border-radius:.5rem;
-          // //   // display:flex;
-          // //   // flex-direction: row-reverse;
-          // //   // padding:.5rem;
-          // //   // margin:0 .5rem;
-          // //   // header{
-          // //   //   // margin-left:.5rem;
-          // //   //   // h3{
-          // //   //   //   margin:0;
-          // //   //   // }
-          // //   //   p{
-          // //   //     font-size:.8rem;
-          // //   //     line-height: .8rem;
-          // //   //   }
-          // //   // }
-          // //   // picture{
-          // //   //   padding:.25rem;
-          // //   //   background: var(--font-primary-color);
-          // //   //   display:flex;
-          // //   //   align-items: center;
-          // //   //   justify-content: center;
-          // //   //   border-radius:50%;
-          // //   // }
-          // }
-        }
       }
       .techCloud{
         display: flex;
@@ -336,6 +180,7 @@
           list-style-type: none;
           max-width: 60vw;
           padding-right:2rem;
+          justify-content: center;
           @mixin text-contrast($n) {
             $color-brightness: round((red($n) * 299) + (green($n) * 587) + calc((blue($n) * 114) / 1000));
             $light-color: round((red(#ffffff) * 299) + (green(#ffffff) * 587) + calc((blue(#ffffff) * 114) / 1000));
@@ -384,18 +229,7 @@
     background:var(--dark-rocks);
     display:flex;
     flex-direction: column;
-    // margin-top:calc(10vh + 5px);
-    // .stopper{
-    //   position:absolute;
-    //   background-image: url("/img/frontPage/projects_footer.webp");
-    //   background-size:auto 100%;
-    //   background-repeat: no-repeat;
-    //   background-position: center;
-    //   height:15rem;
-    //   width:100%;
-    // }
     > header{
-      // margin-top:13rem;
       h2{
         padding-left:1rem;
         font-size:3rem;
@@ -409,27 +243,17 @@
         text-align: center;
         flex:1;
       }
-
     }
     .content{
       display:flex;
       flex-direction: column;
       align-items: center;
-      // p{
-      //   flex:1;
-      //   // margin:0 1.5rem 0rem 1.5rem;
-      //   text-align: center;
-      // }
       section{
         padding-bottom:0;
         width:80%;
-        // @extend %rowSection;
       }
 
     }
-    // .top{
-    //   top:calc(-10vh - 4px);
-    // }
   }
   #business{
     background: var(--bright-primary);
@@ -453,7 +277,6 @@
       line-height:1.5rem;
     }
     section{
-      // @extend %rowSection;
       padding:0 0 2rem 0;
       a{
         margin-top:0;
@@ -475,18 +298,12 @@
       }
       .techCloud ul{
         max-width:100%;
-        justify-content: center;
       }
       >section{
         flex-direction: column;
         padding:0 2rem;
         a{ max-width: 60%; }
       }
-      // footer{
-      //   // text-align: center;
-      //   justify-content: center;
-      //   padding:0;
-      // }
     }
   }
 </style>
