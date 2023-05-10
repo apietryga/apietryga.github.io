81a2adac3a107d3a957357909b6c622599e3e518
apietryga <antek.pietryga@gmail.com>
2023-05-10 22:51:12 +0200

    cursor gradient done

--- app.vue
+++ app.vue
@@ -12,8 +12,16 @@
 <script>
 export default{
   data(){
-    return{
-      window
+    return{ window }
+  },
+  methods: {
+    startTrackin(e){
+      // console.log({ e, c: this.$refs.blob })
+      // const x = e.clientX;
+      // const y = e.clientY;
+      // console.log()
+      // cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+      this.$refs.blob.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
     }
   },
   mounted() {
@@ -22,17 +30,39 @@ export default{
 //     var cursor = this.$refs.blob
     
 //     // document.addEventListener('mousemove', function(e){
-//     this.window.document.addEventListener('mousemove', function(e){
-//       console.log({ e })
-// //       var x = e.clientX;
-// //       var y = e.clientY;
-// //       // cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-//     });
+    this.window.document.addEventListener('mousemove', this.startTrackin);
   },
+  unmounted(){
+    this.window.document.removeEventListener('mousemove', this.startTrackin);
+
+  }
 }
 </script>
 
 <style>
+/* CURSOR GRADIENT */
+.blob {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 650px;
+  height: 650px;
+  border-radius: 100%;
+  /* background-image: linear-gradient(#1100ff 10%, #ff00f2); */
+  background-image: linear-gradient(var(--accent) 10%, var(--primary-color));
+  filter: blur(250px);
+  /* transition: all 450ms ease-out; */
+  position: fixed;
+  pointer-events: none;
+  left: 0;
+  top: 0;
+  transform: translate(calc(-50% + 15px), -50%);
+  z-index: 0;
+}
+
+
+
+/* ANIMATION */
 .page-enter-active,
 .page-leave-active{
   transition: 0.3s;
