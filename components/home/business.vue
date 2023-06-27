bb28c32515ba618d875aca5fa996aa87f3913b85
apietryga <antek.pietryga@gmail.com>
2023-06-27 16:15:32 +0200

    business layout fix

--- components/home/business.vue
+++ components/home/business.vue
@@ -53,27 +53,13 @@
 <script></script>
 
 <style lang="scss">
-.bgWrapper{
-  flex:1;
-  display:flex;
-  justify-content: flex-end;
-  .bgImage{
-    width:100%;
-    height:100px;
-    background-size: cover;
-  }
-}
-.textWrapper{
-  padding:1rem;
-  p{
-    line-height: 1rem;
-    margin-bottom:.5rem !important;
-  }
-}
 #business{
   display:flex;
   flex-wrap: wrap;
   margin:5vh 1rem;
+  max-width: 60%;
+  margin:auto;
+  margin-bottom:5rem;
   >*{
     margin-right:1rem;
     margin-top:1rem;
@@ -91,14 +77,29 @@
       margin-bottom: 1.2rem;
     }
   }
+  .bgWrapper{
+    flex:1;
+    display:flex;
+    justify-content: flex-end;
+    .bgImage{
+      width:100%;
+      height:100px;
+      background-size: cover;
+    }
+  }
   .content{ 
     flex:1;
     display:flex;
     flex-direction: column;
-    background-image: url("/img/contents/uflash_praca.webp");
-    background-size: 100% auto;
+    background-image: url("/img/contents/uflash_home.webp");
+    background-size: auto 100% ;
     background-repeat: no-repeat;
-    background-position: top;
+    background-position: left;
+    .textWrapper{
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+    }
   }
   footer{
     flex:1;
@@ -111,6 +112,19 @@
     .bgWrapper{
       align-items: flex-end;
     }    
+  }
+  
+  .textWrapper{
+    padding:1rem;
+    h3, p{
+      width:fit-content;
+      background-color: rgba(0, 0, 0, 0.836);
+      padding:0 .5rem;
+    }
+    p{
+      line-height: 1rem;
+      margin-bottom:.5rem !important;
+    }
   }
 }
 @media (max-width:768px){
